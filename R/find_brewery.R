e7bee1420d609d60dc732aeaf249ae79a4d20ed1
warlicks <warlick.sean@gmail.com>
2017-11-18 15:53:13 -0800

    Added basic documentation to the find_brewery function.

--- R/find_brewery.R
+++ R/find_brewery.R
@@ -1,16 +1,25 @@
-#' Title
+#' Find a brewery in BreweryDB
 #'
-#' @param api_key
-#' @param name
+#' @param api_key a charater string containing a BreweryDB API key.
+#' See \url{http://www.brewerydb.com/developers} to obtain an API key.
+#' @param name a character string containing the name of a brewery.
 #' @param ids
-#' @param established
-#' @param status
-#' @param data_frame
+#' @param established a character string containing the year indicating the year
+#'  a brewery or breweries where established.  Must follow the format 'YYYY'.
+#' @param status a character string indicating the status of an entity in BreweryDb.  Defaults to \emph{verified}.  Other possible status are
+#' \emph{new_new_unverified}, \emph{update_pending}, \emph{delete_pending} and
+#' \emph{deleted}
+#' @param data_frame a logical argument indicating if the function should return
+#' a data frame.  Defaults to \code{TRUE}.
 #'
-#' @return
+#' @return By default the function returns a data frame.  If
+#' \code{data_frame = FALSE} the function returns the response object created
+#' by \code{\link[httr]{GET}}
 #' @export
 #'
 #' @examples
+
+# Define the function
 find_brewery <- function(api_key,
                          name = NULL,
                          ids = NULL,
@@ -36,10 +45,13 @@ find_brewery <- function(api_key,
     api_call <- httr::GET(query_url)
     httr::stop_for_status(api_call)
 
+    # If the data frame argument is true we use the json_parse function to return
+    # a data frame.  Other wise we return the raw API response.
     if (data_frame) {
         df <- json_parse(api_call)
         return(df)
     } else {
         return(api_call)
     }
+
 }
