8b36d94c3e9d191b671a5c253d4f564617d607fc
warlicks <warlick.sean@gmail.com>
2017-12-01 21:20:10 -0800

    Reworte the function to work the the api responses stored in a list.  This allows us to more easily work with multiple pages.  During development of the function I found a bug where the data frame had columns that were data frames.  We now use flatten from jsonlite to unnest the data frames.

--- R/json_parse.R
+++ R/json_parse.R
@@ -1,6 +1,6 @@
 #' Parse JSON response and return a data frame.
 #'
-#' @param api_content an object of class "response" from \code{\link[httr]{GET}}
+#' @param api_content a list.  Each object in the list should of class "response" from \code{\link[httr]{GET}}
 #'
 #' @return returns a data frame
 #'
@@ -8,11 +8,24 @@
 #'
 #' @examples
 
-json_parse <- function(api_content){
+json_parse <- function(response_list){
     # Convert the api content to raw JSON text
-    raw_json <- httr::content(api_content, as = 'text', encoding = 'UTF-8')
+    all_json <- lapply(response_list,
+                       httr::content,
+                       as = 'text',
+                       encoding = 'UTF-8')
 
-    # Convert json to a data frame
-    df <- jsonlite::fromJSON(raw_json, simplifyDataFrame = TRUE)$data
-    return(df)
+    # Convert elements to data frame
+    all_df <- lapply(all_json, jsonlite::fromJSON, simplifyDataFrame = TRUE)
+
+    # Extract the data frame portion of the response
+    all_df2 <- lapply(all_df, function(x){x[['data']]})
+
+    # Flatten the nested data frames
+    all_flattend_df <- lapply(all_df2, jsonlite::flatten)
+
+    # Bind all the data frames together
+    final_df <- dplyr::bind_rows(all_flattend_df)
+
+    return(final_df)
 }
