508074a62ef2dc7488a7617498ca099620c562fa
warlicks <warlick.sean@gmail.com>
2017-12-01 22:16:45 -0800

    Updated the top level functions to handle multiple pages.  In find locations and brewery multiple pages are spossible and we have an internal funciton to handle this.  Now if data frame is false returns a list.  Cataegories and styles don't have multiple pages of results, but also return a list if data frame is false for consistency.

--- R/find_brewery.R
+++ R/find_brewery.R
@@ -12,9 +12,10 @@
 #' @param data_frame a logical argument indicating if the function should return
 #' a data frame.  Defaults to \code{TRUE}.
 #'
-#' @return By default the function returns a data frame.  If
-#' \code{data_frame = FALSE} the function returns the response object created
-#' by \code{\link[httr]{GET}}
+#'  @return By default the function returns a data frame.  If
+#' \code{data_frame = FALSE} the function returns a list of responses - 1 for each page of results - created by
+#' \code{\link[httr]{GET}}
+#'
 #' @export
 #'
 #' @examples
@@ -54,13 +55,23 @@ find_brewery <- function(api_key,
     api_call <- httr::GET(query_url)
     httr::stop_for_status(api_call)
 
-    # If the data frame argument is true we use the json_parse function to return
-    # a data frame.  Other wise we return the raw API response.
+    # Check the number of pages in the result
+    total_pages <- httr::content(api_call)$numberOfPages
+
+    if(total_pages > 1){
+        api_response <- get_multiple_pages(total_pages, api_call)
+    }
+    else{
+        api_response <- list(page1 = api_call)
+    }
+
+    # If the data frame argument is true we use the json_parse function to
+    # return a data frame.  Other wise we return the raw API response.
     if (data_frame) {
-        df <- json_parse(api_call)
+        df <- json_parse(api_response)
         return(df)
     } else {
-        return(api_call)
+        return(api_response)
     }
 
 }
