d9624b7eddff1b1f7260c37725690ed55ca8ab2a
warlicks <warlick.sean@gmail.com>
2017-12-24 12:05:44 -0800

    inital commit of a function to check that data is actuall returned for a given api call.

---
+++ R/check_for_data_response.R
@@ -0,0 +1,21 @@
+#' Title
+#'
+#' @param api_response
+#'
+#' @return A logical object indicating if data is part of the response object
+#' @keywords internal
+#'
+
+check_for_data_response <- function(api_response){
+    # Set Up error message
+    message <- paste0('No data found for the provided query paramenters. ',
+                  'Check the provided query parameters.')
+
+    # Find names of the objects included in the API response
+    response_objects_names <- names(httr::content(api_response))
+
+
+    assertthat::assert_that(
+        assertthat::has_name(response_objects_names, 'data'),
+        msg = message)
+}
