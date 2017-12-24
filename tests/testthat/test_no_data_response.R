a0d2cfe09feafa71d802a00ead7fde42dbdc3e63
warlicks <warlick.sean@gmail.com>
2017-12-24 13:19:46 -0800

    Added test for find categories.

--- tests/testthat/test_no_data_response.R
+++ tests/testthat/test_no_data_response.R
@@ -12,5 +12,8 @@ test_that('An Error Is Returned When No Data is Returned',{
   expect_error(find_locations(key, locality = 'Fort Dodge'),
                error)
 
+  expect_error(find_categories(key, name = 'Seattle Style'),
+               error)
+
   #expect_error(find_beer(key, name = ))
 })
