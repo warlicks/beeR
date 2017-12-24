8c0f475bc6fecad61b11ccdd823e92268e3aadbd
warlicks <warlick.sean@gmail.com>
2017-12-24 12:54:39 -0800

    inital commit of unit test for no data response.

---
+++ tests/testthat/test_no_data_response.R
@@ -0,0 +1,16 @@
+library(beeR)
+
+context('Check When No Data Is Returned')
+
+key <- Sys.getenv("BREWERY_DB")
+error <- 'No data found for the provided query paramenters'
+
+test_that('An Error Is Returned When No Data is Returned',{
+  expect_error(find_brewery(key, name = 'Ravena'),
+               error)
+
+  expect_error(find_locations(key, locality = 'Fort Dodge'),
+               error)
+
+  #expect_error(find_beer(key, name = ))
+})
