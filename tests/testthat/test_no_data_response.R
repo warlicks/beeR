e42a75a155c28bcf1d20b07ef57b223021d12197
warlicks <warlick.sean@gmail.com>
2017-12-30 17:53:30 -0800

    Added unit tests to check that  beer endpoint and the beer ingredient endpoints handle no data response correctly.

--- tests/testthat/test_no_data_response.R
+++ tests/testthat/test_no_data_response.R
@@ -11,6 +11,11 @@ test_that('An Error Is Returned When No Data is Returned',{
 
   expect_error(find_locations(key, locality = 'Fort Dodge'),
                error)
+
+  expect_error(find_beer(key, name = "Sean's Fake Beer"), error)
+
+  expect_error(find_beer_ingredients(key, '2zmsbt'), error)
+
   ## This test is not needed.  When we put in a fake category the API returns all
   ## categories rather than no data.
   #expect_error(find_categories(key, name = 'Seattle Style'), error)
@@ -19,5 +24,4 @@ test_that('An Error Is Returned When No Data is Returned',{
   ## categories rather than no data.
   #expect_error(find_styles(key, style_name = 'No Name Beer' ),error)
 
-  #expect_error(find_beer(key, name = ))
 })
